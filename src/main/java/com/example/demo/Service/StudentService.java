dba4011e46d4f74a87f728b77a831f56a3bf2398
bala-amypo <bala@amypo.in>
2025-12-17 06:35:44 +0000

    User commit

---
+++ src/main/java/com/example/demo/Service/StudentService.java
@@ -0,0 +1,8 @@
+package com.example.demo.services;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.sterrotype.Service;
+import com.example.demo.entity.StudentEntity;
+@Service
+public class StudentService
